fe9cdd9406a9a1aa9b96392a08a8f906833a5f26
Ericawanja <wanjaerica@gmail.com>
2022-10-26 23:08:49 +0300

    delete and Onchange method

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,41 +4,55 @@ import { BiEditAlt } from "react-icons/bi";
 import todo_data from "./data";
 import { useEffect, useState } from "react";
 function Main() {
-  const [list, setList]=  useState(todo_data)
-  
-  const checkboxChange = (index)=>{
-    list[index].status = list[index].status === 'pending' ? 'done': 'pending'
-    console.log(list[index].status)
-    setList(list)
-    console.log(list[index].status)
+  const [list, setList] = useState(todo_data);
 
-  }
+  const checkboxChange = (id) => {
+    let items = list.map((item) => {
+      if (item.id === id) {
+        item.status = item.status === "pending" ? "done" : "pending";
+      }
+      return item;
+    });
+    setList(items);
+  };
+  const handleDelete = (id) => {
+    const items = list.filter((list_item) => list_item.id != id);
+    setList(items);
+  };
   return (
     <div className="mainWrapper">
       {list.map((item, index) => {
-        const {id,title, desc, status, priority} = item;
+        const { id, title, desc, status, priority } = item;
 
         return (
-          <div className={`task ${priority}`}  key={id}>
+          <div className={`task ${priority}`} key={id}>
             <div className="side">
-      
               <span className="input">
-                <input type="checkbox" name="state" className="checkbox" checked= {status === 'pending' ? false : true} onChange = {()=> checkboxChange(index) }/>
+                <input
+                  type="checkbox"
+                  name="state"
+                  className="checkbox"
+                  checked={status === "pending" ? false : true}
+                  onChange={() => checkboxChange(id)}
+                />
               </span>
-              
             </div>
-            <div className="taskDetails"> 
+            <div className="taskDetails">
               <span className="title">{title}</span>
               <span className="desc">{desc}</span>
             </div>
             <div className="edit_delete_icons">
               <span className="edit">
-                <IconContext.Provider value={{ color: "#f6f8fb", size: "30px" }}>
+                <IconContext.Provider
+                  value={{ color: "#f6f8fb", size: "30px" }}
+                >
                   <BiEditAlt />
                 </IconContext.Provider>
               </span>
-              <span className="delete">
-                <IconContext.Provider value={{ color: "#f6f8fb", size: "30px" }}>
+              <span className="delete" onClick={() => handleDelete(id)}>
+                <IconContext.Provider
+                  value={{ color: "#f6f8fb", size: "30px" }}
+                >
                   <RiDeleteBin6Fill />
                 </IconContext.Provider>
               </span>
